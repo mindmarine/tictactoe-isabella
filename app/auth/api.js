fcd8937fa0ac41a2c598775e72c0062ab434881f
mindmarine <alex.drogeanu@gmail.com>
2021-07-19 09:24:41 -0400

    minor commit code updated

--- app/auth/api.js
+++ app/auth/api.js
@@ -5,50 +5,49 @@ const appCurrentLink = require('./../config')
 const store = require('./../store')
 
 const signUp = function (data) {
-	// console.log(data)
-    // console.log(`This is the current link ${appCurrentLink.apiUrl}`)
-	return $.ajax({
-		url: `${appCurrentLink.apiUrl}/sign-up`,
-		method: 'POST',
-		data: data,
-	})
+  // console.log(data)
+  // console.log(`This is the current link ${appCurrentLink.apiUrl}`)
+  return $.ajax({
+    url: `${appCurrentLink.apiUrl}/sign-up`,
+    method: 'POST',
+    data: data
+  })
 }
 
 const signIn = function (data) {
-	console.log(data)
-	return $.ajax({
-		url: `${appCurrentLink.apiUrl}/sign-in`,
-		method: 'POST',
-		data: data,
-	})
+  console.log(data)
+  return $.ajax({
+    url: `${appCurrentLink.apiUrl}/sign-in`,
+    method: 'POST',
+    data: data
+  })
 }
 
 const signOut = function () {
-	return $.ajax({
-		url: `${appCurrentLink.apiUrl}/sign-out`,
-		method: 'DELETE',
-		headers: {
-			Authorization: 'Bearer ' + store.token,
-		},
-	})
+  return $.ajax({
+    url: `${appCurrentLink.apiUrl}/sign-out`,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + store.token
+    }
+  })
 }
 
 const ChangePassword = function (data) {
-	return $.ajax({
-		url: `${appCurrentLink.apiUrl}/change-password`,
-		method: 'PATCH',
-		headers: {
-			Authorization: 'Bearer ' + store.token,
-		},
-		data: data,
-	})
+  return $.ajax({
+    url: `${appCurrentLink.apiUrl}/change-password`,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Bearer ' + store.token
+    },
+    data: data
+  })
 }
-
 
 // Exports from api.js
 module.exports = {
-	signUp: signUp,
-	signIn: signIn,
-	signOut: signOut,
-	ChangePassword: ChangePassword
+  signUp: signUp,
+  signIn: signIn,
+  signOut: signOut,
+  ChangePassword: ChangePassword
 }
