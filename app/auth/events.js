fa334e3ad373df6062bbda75ed3ddf8cbc1f4254
mindmarine <alex.drogeanu@gmail.com>
2021-07-19 08:52:45 -0400

    Minor commit as VSC wanted to change the files

--- app/auth/events.js
+++ app/auth/events.js
@@ -6,51 +6,51 @@ const api = require('./api')
 const ui = require('./ui')
 
 const onSignUp = function (event) {
-	event.preventDefault()
-    // console.log('This is working on events for onSignUp function')
-    const form = event.target
-    // console.log(form)
-	const data = getFormFields(form)
-    // console.log(data)
-    api.signUp(data)
-        .then(ui.onSignUpSuccess)
-        .catch(ui.onSignUpFailure)
+  event.preventDefault()
+  // console.log('This is working on events for onSignUp function')
+  const form = event.target
+  // console.log(form)
+  const data = getFormFields(form)
+  // console.log(data)
+  api.signUp(data)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
 }
 
 const onSignIn = function (event) {
-	event.preventDefault()
-	// console.log('This is working on events for onSignIn function')
-	const form = event.target
-	// console.log(form)
-	const data = getFormFields(form)
-	// console.log(data)
-	api.signIn(data)
-        .then(ui.onSignInSuccess)
-        .catch(ui.onSignInFailure)
+  event.preventDefault()
+  // console.log('This is working on events for onSignIn function')
+  const form = event.target
+  // console.log(form)
+  const data = getFormFields(form)
+  // console.log(data)
+  api.signIn(data)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignInFailure)
 }
 
 const onSignOut = function () {
-	api.signOut()
-        .then(ui.onSignOutSuccess)
-        .catch(ui.onSignOutFailure)
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
 }
 
 const onChangePassword = function (event) {
-	event.preventDefault()
-	// console.log('This is working on events for onChangePassword function')
-	const form = event.target
-	// console.log(form)
-	const data = getFormFields(form)
-	// console.log(data)
-	api.ChangePassword(data)
-		.then(ui.onChangePasswordSuccess)
-		.catch(ui.onChangePasswordFailure)
+  event.preventDefault()
+  // console.log('This is working on events for onChangePassword function')
+  const form = event.target
+  // console.log(form)
+  const data = getFormFields(form)
+  // console.log(data)
+  api.ChangePassword(data)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onChangePasswordFailure)
 }
 
 // Exports from events.js
 module.exports = {
-	onSignUp: onSignUp,
-	onSignIn: onSignIn,
-	onSignOut: onSignOut,
-	onChangePassword: onChangePassword
-} 
+  onSignUp: onSignUp,
+  onSignIn: onSignIn,
+  onSignOut: onSignOut,
+  onChangePassword: onChangePassword
+}
--- app/auth/ui.js
+++ app/auth/ui.js
@@ -3,55 +3,55 @@
 const store = require('./../store')
 
 const onSignUpSuccess = function (response) {
-	// console.log('Super')
-	$('#status').text('Thank you for signing up', response.user.email)
-	// console.log(response)
-	$('sign-up').trigger('reset')
+  // console.log('Super')
+  $('#status').text('Thank you for signing up', response.user.email)
+  // console.log(response)
+  $('sign-up').trigger('reset')
 }
 
 const onSignUpFailure = function () {
-	console.log('Not possible')
-	$('#status').text('Sign up failure')
-	$('sign-up').trigger('reset')
+  console.log('Not possible')
+  $('#status').text('Sign up failure')
+  $('sign-up').trigger('reset')
 }
 
 const onSignInSuccess = function (response) {
-	// console.log('Super')
-	$('#status').text('Thank you for signing in', response.user.email)
-	// console.log(response)
-    store.token = response.user.token
-	$('sign-in').trigger('reset')
+  // console.log('Super')
+  $('#status').text('Thank you for signing in', response.user.email)
+  // console.log(response)
+  store.token = response.user.token
+  $('sign-in').trigger('reset')
 }
 
 const onSignInFailure = function () {
-	console.log('Not possible')
-	$('#status').text('Sign in failure')
-	$('sign-in').trigger('reset')
+  console.log('Not possible')
+  $('#status').text('Sign in failure')
+  $('sign-in').trigger('reset')
 }
 
 const onSignOutSuccess = function () {
-	$('#status').text('Successfully Signed Out')
+  $('#status').text('Successfully Signed Out')
 }
 
 const onSignOutFailure = function () {
-	$('#status').text('Sign out failure')
+  $('#status').text('Sign out failure')
 }
 
 const onChangePasswordSuccess = function () {
-	$('#status').text('You have changed your password. Please remember the new password')
+  $('#status').text('You have changed your password. Please remember the new password')
 }
 
 const onChangePasswordFailure = function () {
-	$('#status').text('Password change was unsuccessful. Please try again')
+  $('#status').text('Password change was unsuccessful. Please try again')
 }
 
 module.exports = {
-	onSignUpSuccess: onSignUpSuccess,
-	onSignUpFailure: onSignUpFailure,
-	onSignInSuccess: onSignInSuccess,
-	onSignInFailure: onSignInFailure,
-	onSignOutSuccess: onSignOutSuccess,
-	onSignOutFailure: onSignOutFailure,
-	onChangePasswordSuccess: onChangePasswordSuccess,
-	onChangePasswordFailure: onChangePasswordFailure
+  onSignUpSuccess: onSignUpSuccess,
+  onSignUpFailure: onSignUpFailure,
+  onSignInSuccess: onSignInSuccess,
+  onSignInFailure: onSignInFailure,
+  onSignOutSuccess: onSignOutSuccess,
+  onSignOutFailure: onSignOutFailure,
+  onChangePasswordSuccess: onChangePasswordSuccess,
+  onChangePasswordFailure: onChangePasswordFailure
 }
