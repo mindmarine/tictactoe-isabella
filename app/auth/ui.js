28e53134872e8095b54e07dc991dbfa56563a85d
mindmarine <alex.drogeanu@gmail.com>
2021-07-20 19:07:43 -0400

    UX looks Okish

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -60,6 +60,7 @@ const onChangePasswordFailure = function () {
 const onCreateGameSuccess = function (response) {
   $('#status').text('A new game was started')
   $('.square').text('')
+  // $('.board').show()
   // console.log(response)
   // console.log(response.game._id)
   store.gameId = response.game._id
